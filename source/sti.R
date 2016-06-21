e3453103702ac6cc0d6821e3052b504dbcb66a99
smjenness <sjenness@uw.edu>
2016-06-21 14:51:54 -0400

    Allow for dual-site infection of same STI

--- source/sti.R
+++ source/sti.R
@@ -52,9 +52,9 @@ sti_trans <- function(dat, at) {
 
   # rectal GC infection
   # requires urethral GC in infected partner, infected insertive, and no rGC in sus partner
-  p1Inf_rgc <- which(uGC[al[, "p1"]] == 1 & uGC.infTime[al[, "p1"]] < at & uGC[al[, "p2"]] == 0 &
+  p1Inf_rgc <- which(uGC[al[, "p1"]] == 1 & uGC.infTime[al[, "p1"]] < at &
                      rGC[al[, "p2"]] == 0 & al[, "ins"] %in% c(1, 2))
-  p2Inf_rgc <- which(uGC[al[, "p1"]] == 0 & uGC.infTime[al[, "p2"]] < at & uGC[al[, "p2"]] == 1 &
+  p2Inf_rgc <- which(uGC[al[, "p1"]] == 0 & uGC.infTime[al[, "p2"]] < at &
                      rGC[al[, "p1"]] == 0 & al[, "ins"] %in% c(0, 2))
   allActs_rgc <- c(p1Inf_rgc, p2Inf_rgc)
 
@@ -78,9 +78,9 @@ sti_trans <- function(dat, at) {
 
   # urethral GC infection
   # requires rectal GC in infected partner, infected receptive, and no urthethralGC in sus partner
-  p1Inf_ugc <- which(rGC[al[, "p1"]] == 1 & rGC.infTime[al[, "p1"]] < at & rGC[al[, "p2"]] == 0 &
+  p1Inf_ugc <- which(rGC[al[, "p1"]] == 1 & rGC.infTime[al[, "p1"]] < at &
                      uGC[al[, "p2"]] == 0 & al[, "ins"] %in% c(0, 2))
-  p2Inf_ugc <- which(rGC[al[, "p1"]] == 0 & rGC.infTime[al[, "p2"]] < at & rGC[al[, "p2"]] == 1 &
+  p2Inf_ugc <- which(rGC[al[, "p1"]] == 0 & rGC.infTime[al[, "p2"]] < at &
                      uGC[al[, "p1"]] == 0 & al[, "ins"] %in% c(1, 2))
   allActs_ugc <- c(p1Inf_ugc, p2Inf_ugc)
 
@@ -109,9 +109,9 @@ sti_trans <- function(dat, at) {
 
   # rectal CT infection
   # requires urethral CT in infected partner, infected insertive, and no rCT in sus partner
-  p1Inf_rct <- which(uCT[al[, "p1"]] == 1 & uCT.infTime[al[, "p1"]] < at & uCT[al[, "p2"]] == 0 &
+  p1Inf_rct <- which(uCT[al[, "p1"]] == 1 & uCT.infTime[al[, "p1"]] < at &
                      rCT[al[, "p2"]] == 0 & al[, "ins"] %in% c(1, 2))
-  p2Inf_rct <- which(uCT[al[, "p1"]] == 0 & uCT.infTime[al[, "p2"]] < at & uCT[al[, "p2"]] == 1 &
+  p2Inf_rct <- which(uCT[al[, "p1"]] == 0 & uCT.infTime[al[, "p2"]] < at &
                      rCT[al[, "p1"]] == 0 & al[, "ins"] %in% c(0, 2))
   allActs_rct <- c(p1Inf_rct, p2Inf_rct)
 
@@ -134,9 +134,9 @@ sti_trans <- function(dat, at) {
 
   # urethral CT infection
   # requires rectal CT in infected partner, infected receptive, and no urthethralCT in sus partner
-  p1Inf_uct <- which(rCT[al[, "p1"]] == 1 & rCT.infTime[al[, "p1"]] < at & rCT[al[, "p2"]] == 0 &
+  p1Inf_uct <- which(rCT[al[, "p1"]] == 1 & rCT.infTime[al[, "p1"]] < at &
                      uCT[al[, "p2"]] == 0 & al[, "ins"] %in% c(0, 2))
-  p2Inf_uct <- which(rCT[al[, "p1"]] == 0 & rCT.infTime[al[, "p2"]] < at & rCT[al[, "p2"]] == 1 &
+  p2Inf_uct <- which(rCT[al[, "p1"]] == 0 & rCT.infTime[al[, "p2"]] < at &
                      uCT[al[, "p1"]] == 0 & al[, "ins"] %in% c(1, 2))
   allActs_uct <- c(p1Inf_uct, p2Inf_uct)
 
