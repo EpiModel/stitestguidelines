5309110842a92f4841f9ce5c51e62f7d4c994360
smjenness <sjenness@uw.edu>
2016-06-28 12:21:29 -0400

    Update STI incidence vectors

--- source/sti.R
+++ source/sti.R
@@ -224,10 +224,12 @@ sti_trans <- function(dat, at) {
   dat$attr$CT.cease <- CT.cease
 
   # Summary stats
-  dat$epi$incid.rgc <- length(idsInf_rgc)
-  dat$epi$incid.ugc <- length(idsInf_ugc)
-  dat$epi$incid.rct <- length(idsInf_rct)
-  dat$epi$incid.uct <- length(idsInf_uct)
+  dat$epi$incid.rgc[at] <- length(idsInf_rgc)
+  dat$epi$incid.ugc[at] <- length(idsInf_ugc)
+  dat$epi$incid.gc[at] <- length(idsInf_rgc) + length(idsInf_ugc)
+  dat$epi$incid.rct[at] <- length(idsInf_rct)
+  dat$epi$incid.uct[at] <- length(idsInf_uct)
+  dat$epi$incid.ct[at] <- length(idsInf_rct) + length(idsInf_uct)
 
   # Check all infected have all STI attributes
   stopifnot(all(!is.na(rGC.infTime[rGC == 1])),
