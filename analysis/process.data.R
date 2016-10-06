59f1667bba63c7406c9e9de5f927f6c3ff98703b
Samuel Jenness <sjenness@uw.edu>
2016-10-06 15:53:14 -0400

    Fix issues with data processing

--- analysis/process.data.R
+++ analysis/process.data.R
@@ -15,7 +15,8 @@ system("scp hyak:/gscratch/csde/sjenness/sti/data/sim.n201[0-9].rda data/")
 ( fn <- list.files("data/", full.names = TRUE) )
 
 # Truncate n1000 data
-fn <- list.files("data/", pattern = "n10[0-9][0-9]", full.names = TRUE)[-1]
+fn <- list.files("data/", pattern = "n10[0-9][0-9]", full.names = TRUE)
+fn <- list.files("data/", pattern = "n100.rda", full.names = TRUE)
 for (i in fn) {
   load(i)
   sim <- truncate_sim(sim, at = 2600)
@@ -25,6 +26,7 @@ for (i in fn) {
 
 # Remove unneeded variables n1000 data
 fn <- list.files("data/", pattern = "n10[0-9][0-9]", full.names = TRUE)
+fn <- list.files("data/", pattern = "n100.rda", full.names = TRUE)
 for (i in fn) {
   load(i)
   vars.needed <- c("ir100", "incid", "ir100.gc", "incid.gc", "ir100.ct", "incid.ct",
@@ -36,10 +38,42 @@ for (i in fn) {
 }
 
 # Truncate n2000 data
-fn <- list.files("data/", pattern = "n201[0-9]", full.names = TRUE)
+fn <- list.files("data/", pattern = "n200[0-9]", full.names = TRUE)
 for (i in fn) {
   load(i)
   sim <- truncate_sim(sim, at = 2600)
   save(sim, file = i, compress = TRUE)
   cat("*")
+}
+
+# Remove unneeded variables n2000 data
+fn <- list.files("data/", pattern = "n200[0-9]", full.names = TRUE)
+for (i in fn) {
+  load(i)
+  vars.needed <- c("ir100", "incid", "ir100.gc", "incid.gc", "ir100.ct", "incid.ct",
+                   "prepCurr", "i.prev", "prev.gc", "prev.ct",
+                   "ir100.sti", "ir100.sti.prep", "num.asympt.tx",
+                   "num.asympt.cases", "num.rect.tx", "num.rect.cases")
+  i.vars <- which(names(sim$epi) %in% vars.needed)
+  sim$epi <- sim$epi[i.vars]
+  save(sim, file = i, compress = "xz")
+  cat("\n", i, "complete")
+}
+
+
+# truncate and limit sim 3000/4000 files
+suppressMessages(library("EpiModelHIV"))
+fn <- list.files(pattern = "n[3-4][0-9][0-9][0-9].rda")
+for (i in fn) {
+  load(i)
+  sim <- truncate_sim(sim, at = 2600)
+  vars.needed <- c("ir100", "incid", "ir100.gc", "incid.gc",
+                   "ir100.ct", "incid.ct", "prepCurr",
+                   "ir100.sti", "ir100.sti.prep")
+  i.vars <- which(names(sim$epi) %in% vars.needed)
+  sim$epi <- sim$epi[i.vars]
+  out.fn <- paste0("save/", i)
+  save(sim, file = out.fn, compress = "xz")
+  file.remove(i)
+  cat(i, "\n")
 }
