d8723c829aa417c42b44b6d4e8ebcffc595452cb
kweiss2 <kevin.weiss@emory.edu>
2017-01-25 11:09:10 -0500

    Updated burn-in file

--- scripts/burnin/sim.burn.R
+++ scripts/burnin/sim.burn.R
@@ -17,10 +17,11 @@ for (i in seq_along(mean.p)) {
 }
 
 param <- param_msm(nwstats = st,
+                   ai.scale = 1.11889726,
                    prep.start = 5000,
                    partlist.start = 5000,
                    stitest.start = 5000,
-
+                   
                    hiv.rgc.rr = 2.780673,
                    hiv.ugc.rr = 1.732363,
                    hiv.rct.rr = 2.780673,
@@ -28,14 +29,15 @@ param <- param_msm(nwstats = st,
                    hiv.syph.rr = 2.00793856,
                    syph.hiv.rr = 2.16933746,
                    
-
                    syph.tprob = 0.01950727,
                    rgc.tprob = 0.38353111, # was 0.357698
                    ugc.tprob = 0.25444490, # was 0.248095
                    rct.tprob = 0.31968155, # was 0.321597
                    uct.tprob = 0.23424104, # was 0.212965
-
-                   ai.scale = 1.11889726,
+                   
+                   hiv.syph.rr = 2.00793856,
+                   syph.hiv.rr = 2.16933746,
+                   
                    prep.coverage = 0,
                    ept.coverage = 0,
                    
@@ -44,7 +46,9 @@ param <- param_msm(nwstats = st,
                    rct.sympt.prob = 0.14, # Beck
                    uct.sympt.prob = 0.58, # Beck
                    
-                   stitest.int = 182) # adjustable for 3 or 6 months)
+                   stitest.active.int = 364,
+                   sti.highrisktest.int = 182)
+                   
 init <- init_msm(nwstats = st)
 control <- control_msm(simno = fsimno,
                        nsteps = 3120,
