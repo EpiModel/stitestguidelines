35c9d0a9f75ce1c98c2c7daa3c6fa178fa98759d
kweiss2 <kevin.weiss@emory.edu>
2017-02-02 15:58:42 -0500

    Change ranges for ABC

--- scripts/burnin/sim.burn.R
+++ scripts/burnin/sim.burn.R
@@ -33,26 +33,26 @@ param <- param_msm(nwstats = st,
                    usyph.tprob = 0.030, # 0.01950727,
                    
                    rgc.tprob = 0.40, #0.3928965, # 0.38353111, # was 0.357698 # functional at 0.40
-                   ugc.tprob = 0.30, #0.24297633, # 0.25444490, # was 0.248095 # functional at 0.35
-                   rct.tprob = 0.25, #0.29367628, # 0.31968155, # was 0.321597 # functional at 0.21
-                   uct.tprob = 0.15, #0.25309465,# 0.23424104, # was 0.212965 # functional at 0.15
+                   ugc.tprob = 0.32, #0.24297633, # 0.25444490, # was 0.248095 # functional at 0.35
+                   rct.tprob = 0.21, #0.29367628, # 0.31968155, # was 0.321597 # functional at 0.21
+                   uct.tprob = 0.16, #0.25309465,# 0.23424104, # was 0.212965 # functional at 0.15
                    
                    prep.coverage = 0,
                    ept.coverage = 0,
                    
                    stitest.active.int = 364,
-                   sti.highrisktest.int = 182) # adjustable for 3 or 6 months                   sti.highrisktest.int = 182) # adjustable for 3 or 6 months
+                   sti.highrisktest.int = 182) # adjustable for 3 or 6 months
 
-                   
-init <- init_msm(nwstats = st,
+init <- init_msm(nwstats = st, 
                  prev.B = 0.10, 
                  prev.W = 0.10,
-                 prev.ugc = 0.015,
-                 prev.rgc = 0.015,
-                 prev.uct = 0.015,
-                 prev.rct = 0.015,
-                 prev.syph.B = 0.02,
-                 prev.syph.W = 0.02)
+                 prev.ugc = 0.013,
+                 prev.rgc = 0.013,
+                 prev.uct = 0.013,
+                 prev.rct = 0.013,
+                 prev.syph.B = 0.01,
+                 prev.syph.W = 0.01)
+
 control <- control_msm(simno = fsimno,
                        nsteps = 3120,
                        nsims = 16, ncores = 16,
