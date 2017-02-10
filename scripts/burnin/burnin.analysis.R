665809e42b1e3d0b6522c3873ac3680ac992684c
kweiss2 <kevin.weiss@emory.edu>
2017-02-10 14:47:07 -0500

    Further burn-in analysis updates

--- scripts/burnin/burnin.analysis.R
+++ scripts/burnin/burnin.analysis.R
@@ -10,84 +10,82 @@
 # 
 # ls()
 
-## Prevalence of HIV, GC, CT, Syphilis
-i.prev <- as.numeric(mean(sim$epi$i.prev[2500:2600, ]))
-round(quantile(i.prev, probs = c(0.025, 0.5, 0.975)), 3)
-
-prev.syph <- as.numeric(mean(sim$epi$prev.syph[2500:2600, ]))
-round(quantile(prev.syph, probs = c(0.025, 0.5, 0.975)), 3)
-
-prev.gc <- as.numeric(mean(sim$epi$prev.gc[2500:2600, ]))
-round(quantile(prev.gc, probs = c(0.025, 0.5, 0.975)), 3)
-
-prev.rgc <- as.numeric(mean(sim$epi$prev.rgc[2500:2600, ]))
-round(quantile(prev.rgc, probs = c(0.025, 0.5, 0.975)), 3)
-
-prev.ugc <- as.numeric(mean(sim$epi$prev.ugc[2500:2600, ]))
-round(quantile(prev.ugc, probs = c(0.025, 0.5, 0.975)), 3)
-
-prev.rct <- as.numeric(mean(sim$epi$prev.rct[2500:2600, ]))
-round(quantile(prev.rct, probs = c(0.025, 0.5, 0.975)), 3)
-
-prev.uct <- as.numeric(mean(sim$epi$prev.uct[2500:2600, ]))
-round(quantile(prev.uct, probs = c(0.025, 0.5, 0.975)), 3)
-
-prev.ct <- as.numeric(mean(sim$epi$prev.ct[2500:2600, ]))
-round(quantile(prev.ct, probs = c(0.025, 0.5, 0.975)), 3)
-
-
 ## Incidence
 
-ir100.gc <- as.numeric(mean(sim$epi$ir100.gc[2500:2600, ]))
+ir100.gc <- as.numeric(sim$epi$ir100.gc[2600, ])
 round(quantile(ir100.gc, probs = c(0.025, 0.5, 0.975)), 3)
 
-ir100.ct <- as.numeric(mean(sim$epi$ir100.ct[2500:2600, ]))
+ir100.ct <- as.numeric(sim$epi$ir100.ct[2600, ])
 round(quantile(ir100.ct, probs = c(0.025, 0.5, 0.975)), 3)
 
-ir100 <- as.numeric(mean(sim$epi$ir100[2500:2600, ]))
+ir100 <- as.numeric(sim$epi$ir100[2600, ])
 round(quantile(ir100, probs = c(0.025, 0.5, 0.975)), 3)
 
-ir100.syph <- as.numeric(mean(sim$epi$ir100.syph[2500:2600, ]))
+ir100.syph <- as.numeric(sim$epi$ir100.syph[2600, ])
 round(quantile(ir100.syph, probs = c(0.025, 0.5, 0.975)), 3)
 
+i.prev <- as.numeric(sim$epi$i.prev[2600, ])
+round(quantile(i.prev, probs = c(0.025, 0.5, 0.975)), 3)
 
 # Syphilis stage prevalence and HIV-syphilis overlap
 
-prev.hiv.syphpos <- as.numeric(mean(sim$epi$prev.hiv.syphpos[2500:2600, ]))
-round(quantile(prev.hiv.syphpos, probs = c(0.025, 0.5, 0.975)), 3)
-
-prev.syph.hivpos <- as.numeric(mean(sim$epi$prev.syph.hivpos[2500:2600, ]))
+prev.syph.hivpos <- as.numeric(sim$epi$prev.syph.hivpos[2600, ])
 round(quantile(prev.syph.hivpos, probs = c(0.025, 0.5, 0.975)), 3)
 
-prev.syph.hivneg <- as.numeric(mean(sim$epi$prev.syph.hivneg[2500:2600, ]))
+prev.syph.hivneg <- as.numeric(sim$epi$prev.syph.hivneg[2600, ])
 round(quantile(prev.syph.hivneg, probs = c(0.025, 0.5, 0.975)), 3)
 
-prev.stage.incubprim <- as.numeric(mean(sim$epi$prev.stage.incubprim[2500:2600, ]))
-round(quantile(prev.stage.incubprim, probs = c(0.025, 0.5, 0.975)), 3)
+prev.syph <- as.numeric(sim$epi$prev.syph[2600, ])
+round(quantile(prev.syph, probs = c(0.025, 0.5, 0.975)), 3)
 
-prev.stage.seco <- as.numeric(mean(sim$epi$prev.stage.seco[2500:2600, ]))
-round(quantile(prev.stage.seco, probs = c(0.025, 0.5, 0.975)), 3)
+prev.hiv.syphpos <- as.numeric(sim$epi$prev.hiv.syphpos[2600, ])
+round(quantile(prev.hiv.syphpos, probs = c(0.025, 0.5, 0.975)), 3)
 
-prev.stage.earlat <- as.numeric(mean(sim$epi$prev.stage.earlat[2500:2600, ]))
-round(quantile(prev.stage.earlat, probs = c(0.025, 0.5, 0.975)), 3)
-
-prev.stage.latelat <- as.numeric(mean(sim$epi$prev.stage.latelat[2500:2600, ]))
-round(quantile(prev.stage.latelat, probs = c(0.025, 0.5, 0.975)), 3)
-
-prev.stage.latelatelat <- as.numeric(mean(sim$epi$prev.stage.latelatelat[2500:2600, ]))
-round(quantile(prev.stage.latelatelat, probs = c(0.025, 0.5, 0.975)), 3)
-
-prev.stage.alllatelat <- as.numeric(mean(sim$epi$prev.stage.alllatelat[2500:2600, ]))
-round(quantile(prev.stage.alllatelat, probs = c(0.025, 0.5, 0.975)), 3)
-
-prev.stage.tert <- as.numeric(mean(sim$epi$prev.stage.tert[2500:2600, ]))
-round(quantile(prev.stage.tert, probs = c(0.025, 0.5, 0.975)), 3)
-
-prev.earlysyph <- as.numeric(mean(sim$epi$prev.earlysyph[2500:2600, ]))
+prev.earlysyph <- as.numeric(sim$epi$prev.earlysyph[2600, ])
 round(quantile(prev.earlysyph, probs = c(0.025, 0.5, 0.975)), 3)
 
-prev.latesyph <- as.numeric(mean(sim$epi$prev.latesyph[2500:2600, ]))
+prev.latesyph <- as.numeric(sim$epi$prev.latesyph[2600, ])
 round(quantile(prev.latesyph, probs = c(0.025, 0.5, 0.975)), 3)
+
+prev.stage.incubprim <- as.numeric(sim$epi$prev.stage.incubprim[2600, ])
+round(quantile(prev.stage.incubprim, probs = c(0.025, 0.5, 0.975)), 3)
+
+prev.stage.seco <- as.numeric(sim$epi$prev.stage.seco[2600, ])
+round(quantile(prev.stage.seco, probs = c(0.025, 0.5, 0.975)), 3)
+
+prev.stage.earlat <- as.numeric(sim$epi$prev.stage.earlat[2600, ])
+round(quantile(prev.stage.earlat, probs = c(0.025, 0.5, 0.975)), 3)
+
+prev.stage.latelat <- as.numeric(sim$epi$prev.stage.latelat[2600, ])
+round(quantile(prev.stage.latelat, probs = c(0.025, 0.5, 0.975)), 3)
+
+prev.stage.latelatelat <- as.numeric(sim$epi$prev.stage.latelatelat[2600, ])
+round(quantile(prev.stage.latelatelat, probs = c(0.025, 0.5, 0.975)), 3)
+
+prev.stage.alllatelat <- as.numeric(sim$epi$prev.stage.alllatelat[2600, ])
+round(quantile(prev.stage.alllatelat, probs = c(0.025, 0.5, 0.975)), 3)
+
+prev.stage.tert <- as.numeric(sim$epi$prev.stage.tert[2600, ])
+round(quantile(prev.stage.tert, probs = c(0.025, 0.5, 0.975)), 3)
+
+
+prev.gc <- as.numeric(sim$epi$prev.gc[2600, ])
+round(quantile(prev.gc, probs = c(0.025, 0.5, 0.975)), 3)
+
+prev.rgc <- as.numeric(sim$epi$prev.rgc[2600, ])
+round(quantile(prev.rgc, probs = c(0.025, 0.5, 0.975)), 3)
+
+prev.ugc <- as.numeric(sim$epi$prev.ugc[2600, ])
+round(quantile(prev.ugc, probs = c(0.025, 0.5, 0.975)), 3)
+
+prev.ct <- as.numeric(sim$epi$prev.ct[2600, ])
+round(quantile(prev.ct, probs = c(0.025, 0.5, 0.975)), 3)
+
+prev.rct <- as.numeric(sim$epi$prev.rct[2600, ])
+round(quantile(prev.rct, probs = c(0.025, 0.5, 0.975)), 3)
+
+prev.uct <- as.numeric(sim$epi$prev.uct[2600, ])
+round(quantile(prev.uct, probs = c(0.025, 0.5, 0.975)), 3)
 
 prev.rgcct <- as.numeric(sim$epi$prev.rgcct[2600, ])
 round(quantile(prev.rgcct, probs = c(0.025, 0.5, 0.975)), 3)
@@ -96,38 +94,168 @@ prev.ugcct <- as.numeric(sim$epi$prev.ugcct[2600, ])
 round(quantile(prev.ugcct, probs = c(0.025, 0.5, 0.975)), 3)
 
 # Summary of 500 sims
-par(mfrow = c(3,2), oma=c(0,0,2,0))
-plot(sim, y = "i.prev", ylim = c(0.23, 0.29))
+# Syphilis prevalence plots
+par(mfrow = c(1, 1), oma = c(0,0,2,0))
+plot(sim, y = "prev.syph.hivpos", ylab = "Prevalence", ylim = c(0, 0.15), mean.col = "blue", qnts.col = "blue")
+plot(sim, y = "prev.syph.hivneg", ylab = "Prevalence", add = TRUE, mean.col = "red", qnts.col = "red")
+abline(h = 0.103, col = "blue", lty = 2)
+abline(h = 0.026, col = "red", lty = 2)
+title("Syphilis by HIV Status")
+legend("topleft", c("HIV+", "HIV-"), col = c("blue", "red"), lty = c(1, 1))
+
+plot(sim, y = "prev.stage.incubprim", ylab = "Prevalence", ylim = c(0.00, 0.40), mean.col = "blue", qnts.col = "blue")
+plot(sim, y = "prev.stage.seco", ylab = "Prevalence", add = TRUE, mean.col = "red", qnts.col = "red")
+plot(sim, y = "prev.stage.earlat", ylab = "Prevalence", add = TRUE, mean.col = "purple", qnts.col = "purple")
+abline(h = 0.20, col = "blue", lty = 2)
+abline(h = 0.077, col = "red", lty = 2)
+abline(h = 0.2770, col = "purple", lty = 2)
+title("P, S, and Early latent Syphilis Prevalence")
+legend("topright", c("Primary", "Seco", "Early latent"), col = c("blue", "red", "purple"), lty = c(1, 1, 1))
+
+plot(sim, y = "prev.stage.alllatelat", ylab = "Prevalence", ylim = c(0.00, 0.60), mean.col = "blue", qnts.col = "blue")
+plot(sim, y = "prev.stage.tert", ylab = "Prevalence", add = TRUE, mean.col = "red", qnts.col = "red")
+abline(h = 0.44, col = "blue", lty = 2)
+abline(h = 0.006, col = "red", lty = 2)
+title("Late Latent and Tertiary Syphilis Prevalence")
+legend("right", c("Late latent", "Tertiary"), col = c("blue", "red"), lty = c(1, 1))
+
+# All syphilis stages
+par(mfrow = c(1, 1), oma = c(0, 0, 2, 0))
+plot(sim, y = "prev.stage.incubprim", ylab = "Prevalence", ylim = c(0, 1), mean.col = "blue")
+plot(sim, y = "prev.stage.seco", ylab = "Prevalence", add = TRUE, mean.col = "red")
+plot(sim, y = "prev.stage.earlat", ylab = "Prevalence", add = TRUE, mean.col = "orange")
+plot(sim, y = "prev.stage.alllatelat", ylab = "Prevalence", add = TRUE, mean.col = "purple")
+plot(sim, y = "prev.stage.tert", ylab = "Prevalence", add = TRUE, mean.col = "black")
+title("Stage-specific Syphilis Prevalence")
+abline(h = 0.20, col = "blue", lty = 1)
+abline(h = 0.077, col = "red", lty = 1)
+abline(h = 0.2770, col = "orange", lty = 1)
+abline(h = 0.44, col = "purple", lty = 1)
+abline(h = 0.006, col = "black", lty = 1)
+legend("topright", c("Primary/Incub", "Secondary", "Early Latent", "Late latent", "Tertiary"),
+       col = c("blue", "red", "orange", "purple", "black"), lty = c(1, 1, 1, 1, 1))
+
+# Early vs late syphilis
+par(mfrow = c(1, 1), oma = c(0, 0, 2, 0))
+plot(sim, y = "prev.earlysyph", ylab = "Prevalence", ylim = c(0, 1), mean.col = "blue", qnts.col = "blue")
+plot(sim, y = "prev.latesyph", ylab = "Prevalence", add = TRUE, mean.col = "red", qnts.col = "red")
+title("Early and Late-stage Syphilis")
+abline(h = 0.554, col = "blue", lty = 1)
+abline(h = 0.446, col = "red", lty = 1)
+legend("topright", c("Early", "Late"), col = c("blue", "red"), lty = c(1, 1))
+
+# STI Prevalence
+par(mfrow = c(2,2), oma = c(0,0,2,0))
+plot(sim, y = "prev.syph", ylab = "Prevalence")
+title("Syphilis Prevalence")
+abline(h = 0.046, col = "red", lty = 2)
+plot(sim, y = "prev.ct", ylab = "Prevalence")
+title("CT Prevalence")
+plot(sim, y = "prev.gc", ylab = "Prevalence")
+title("GC Prevalence")
+plot(sim, y = "i.prev", ylim = c(0, 0.3), ylab = "Prevalence")
+abline(h = 0.26, col = "red",  lty = 2)
 title("HIV Prevalence")
-plot(sim, y = "prev.rgcct", ylim = c(0.12, 0.17))
-title("Rectal STI Prevalence")
-plot(sim, y = "prev.ugcct", ylim = c(0.06, 0.11))
-title("Urethral STI Prevalence")
-plot(sim, y = "ir100.gc", ylim = c(18, 28))
-title("Gonococcal Incidence Rate")
-plot(sim, y = "ir100.ct", ylim = c(20, 30))
-title("Chlamydial Incidence Rate")
-plot(sim, y = "ir100", ylim = c(3, 5))
-title("HIV Incidence Rate")
-title(main="Statistics for 500 Simulations", outer=TRUE)
+
+# HIV and STI Incidence
+par(mfrow = c(2,2), oma = c(0,0,2,0))
+plot(sim, y = "ir100")
+abline(h = 3.8, col = "red", lty = 2)
+title("HIV Incidence")
+plot(sim, y = "ir100.gc")
+abline(h = 4.2, col = "red", lty = 2)
+title("GC Incidence")
+plot(sim, y = "ir100.ct")
+abline(h = 6.6, col = "red", lty = 2)
+title("CT Incidence")
+plot(sim, y = "ir100.syph")
+abline(h = 0.9, col = "red", lty = 2)
+title("Syph Incidence")
 
 
+
+###############################
 # Summary of mean simulation
 # Run after burnin.process
-par(mfrow = c(3,2), oma=c(0,0,2,0))
-plot(sim, y = "i.prev", ylim = c(0.23, 0.29))
+###############################
+
+# Syphilis prevalence plots
+par(mfrow = c(1, 1), oma = c(0,0,2,0))
+plot(sim, y = "prev.syph.hivpos", ylab = "Prevalence", ylim = c(0, 0.15), mean.col = "blue", qnts.col = "blue")
+plot(sim, y = "prev.syph.hivneg", ylab = "Prevalence", add = TRUE, mean.col = "red", qnts.col = "red")
+abline(h = 0.103, col = "blue", lty = 2)
+abline(h = 0.026, col = "red", lty = 2)
+title("Syphilis by HIV Status")
+legend("topleft", c("HIV+", "HIV-"), col = c("blue", "red"), lty = c(1, 1))
+
+plot(sim, y = "prev.stage.incubprim", ylab = "Prevalence", ylim = c(0.00, 0.40), mean.col = "blue", qnts.col = "blue")
+plot(sim, y = "prev.stage.seco", ylab = "Prevalence", add = TRUE, mean.col = "red", qnts.col = "red")
+plot(sim, y = "prev.stage.earlat", ylab = "Prevalence", add = TRUE, mean.col = "purple", qnts.col = "purple")
+abline(h = 0.20, col = "blue", lty = 2)
+abline(h = 0.077, col = "red", lty = 2)
+abline(h = 0.2770, col = "purple", lty = 2)
+title("P, S, and Early latent Syphilis Prevalence")
+legend("topright", c("Primary", "Seco", "Early latent"), col = c("blue", "red", "purple"), lty = c(1, 1, 1))
+
+plot(sim, y = "prev.stage.alllatelat", ylab = "Prevalence", ylim = c(0.00, 0.60), mean.col = "blue", qnts.col = "blue")
+plot(sim, y = "prev.stage.tert", ylab = "Prevalence", add = TRUE, mean.col = "red", qnts.col = "red")
+abline(h = 0.44, col = "blue", lty = 2)
+abline(h = 0.006, col = "red", lty = 2)
+title("Late Latent and Tertiary Syphilis Prevalence")
+legend("right", c("Late latent", "Tertiary"), col = c("blue", "red"), lty = c(1, 1))
+
+# All syphilis stages
+par(mfrow = c(1, 1), oma = c(0, 0, 2, 0))
+plot(sim, y = "prev.stage.incubprim", ylab = "Prevalence", ylim = c(0, 1), mean.col = "blue")
+plot(sim, y = "prev.stage.seco", ylab = "Prevalence", add = TRUE, mean.col = "red")
+plot(sim, y = "prev.stage.earlat", ylab = "Prevalence", add = TRUE, mean.col = "orange")
+plot(sim, y = "prev.stage.alllatelat", ylab = "Prevalence", add = TRUE, mean.col = "purple")
+plot(sim, y = "prev.stage.tert", ylab = "Prevalence", add = TRUE, mean.col = "black")
+title("Stage-specific Syphilis Prevalence")
+abline(h = 0.20, col = "blue", lty = 1)
+abline(h = 0.077, col = "red", lty = 1)
+abline(h = 0.2770, col = "orange", lty = 1)
+abline(h = 0.44, col = "purple", lty = 1)
+abline(h = 0.006, col = "black", lty = 1)
+legend("topright", c("Primary/Incub", "Secondary", "Early Latent", "Late latent", "Tertiary"),
+       col = c("blue", "red", "orange", "purple", "black"), lty = c(1, 1, 1, 1, 1))
+
+# Early vs late syphilis
+par(mfrow = c(1, 1), oma = c(0, 0, 2, 0))
+plot(sim, y = "prev.earlysyph", ylab = "Prevalence", ylim = c(0, 1), mean.col = "blue", qnts.col = "blue")
+plot(sim, y = "prev.latesyph", ylab = "Prevalence", add = TRUE, mean.col = "red", qnts.col = "red")
+title("Early and Late-stage Syphilis")
+abline(h = 0.554, col = "blue", lty = 1)
+abline(h = 0.446, col = "red", lty = 1)
+legend("topright", c("Early", "Late"), col = c("blue", "red"), lty = c(1, 1))
+
+# STI Prevalence
+par(mfrow = c(2,2), oma = c(0,0,2,0))
+plot(sim, y = "prev.syph", ylab = "Prevalence")
+title("Syphilis Prevalence")
+abline(h = 0.046, col = "red", lty = 2)
+plot(sim, y = "prev.ct", ylab = "Prevalence")
+title("CT Prevalence")
+plot(sim, y = "prev.gc", ylab = "Prevalence")
+title("GC Prevalence")
+plot(sim, y = "i.prev", ylim = c(0, 0.3), ylab = "Prevalence")
+abline(h = 0.26, col = "red",  lty = 2)
 title("HIV Prevalence")
-plot(sim, y = "prev.rgcct", ylim = c(0.12, 0.17))
-title("Rectal STI Prevalence")
-plot(sim, y = "prev.ugcct", ylim = c(0.06, 0.11))
-title("Urethral STI Prevalence")
-plot(sim, y = "ir100.gc", ylim = c(18, 28))
-title("Gonococcal Incidence Rate")
-plot(sim, y = "ir100.ct", ylim = c(20, 30))
-title("Chlamydial Incidence Rate")
-plot(sim, y = "ir100", ylim = c(3, 5))
-title("HIV Incidence Rate")
-title(main="Statistics for Best-fitting Simulation", outer=TRUE)
+
+# HIV and STI Incidence
+par(mfrow = c(2,2), oma = c(0,0,2,0))
+plot(sim, y = "ir100")
+abline(h = 3.8, col = "red", lty = 2)
+title("HIV Incidence")
+plot(sim, y = "ir100.gc")
+abline(h = 4.2, col = "red", lty = 2)
+title("GC Incidence")
+plot(sim, y = "ir100.ct")
+abline(h = 6.6, col = "red", lty = 2)
+title("CT Incidence")
+plot(sim, y = "ir100.syph")
+abline(h = 0.9, col = "red", lty = 2)
+title("Syph Incidence")
 
 df <- as.data.frame(sim)
 head(df$prev.rgcct, 100)
