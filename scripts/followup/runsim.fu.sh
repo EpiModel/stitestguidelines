dad342be136744aa322d9c91bd45ad3205cd0e81
Kevin Weiss <kevin.weiss@emory.edu>
2018-11-28 16:05:59 -0500

    update spack location

--- scripts/followup/runsim.fu.sh
+++ scripts/followup/runsim.fu.sh
@@ -23,7 +23,7 @@
 #SBATCH --workdir=/suppscr/csde/kweiss2/sti
 
 ### Modules
-. /suppscr/csde/sjenness/spack/share/spack/setup-env.sh
+. /gscratch/csde/sjenness/spack/share/spack/setup-env.sh
 
 module load gcc-8.1.0-gcc-4.4.7-eaajvcy
 module load r-3.5.1-gcc-8.1.0-unb32sy
