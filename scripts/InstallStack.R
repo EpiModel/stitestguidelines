28a74fde36bda287feb242324b6f725cc7ef0ac5
smjenness <smjenness@gmail.com>
2019-01-22 10:42:20 -0500

    Update InstallStack.R

--- scripts/InstallStack.R
+++ scripts/InstallStack.R
@@ -4,18 +4,14 @@
 # Install EpiModel Stack
 install.packages("EpiModel", dependencies = TRUE)
 
-# Install Extra Helper Packages
-install.packages("tidyverse", dep = TRUE)
-install.packages("devtools", dep = TRUE)
-install.packages("remotes", dep = TRUE)
+# Install latest Dev Versions of Packages
+install.packages("remotes")
 
-# Latest Dev Versions of Packages
 remotes::install_github(c("statnet/network",
                           "statnet/statnet.common",
                           "statnet/ergm",
-                          "statnet/tergm"))
-
-remotes::install_github(c("statnet/EpiModel",
+                          "statnet/tergm",
+                          "statnet/EpiModel",
                           "statnet/EpiModelHPC",
                           "statnet/tergmLite",
                           "EpiModel/EpiABC"))
